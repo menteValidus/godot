d16ac00114a5c457ab4181ed604ecd04b52c45fd
Denis Cherny <39064926+menteValidus@users.noreply.github.com>
2023-09-23 16:05:29 +0400

    Write tests for setters

--- tests/scene/test_animated_texture.h
+++ tests/scene/test_animated_texture.h
@@ -43,12 +43,20 @@ TEST_CASE("[AnimatedTexture] Initial configuration") {
     CHECK_EQ(sut->get_frames(), 1);
     CHECK_EQ(sut->get_current_frame(), 0);
     CHECK_FALSE(sut->get_one_shot());
+    CHECK_FALSE(sut->get_pause());
     CHECK_EQ(sut->get_frame_duration(0), 1.0f);
     CHECK_EQ(sut->get_speed_scale(), 1.0f);
     CHECK_EQ(sut->get_width(), 1);
     CHECK_EQ(sut->get_height(), 1);
-    CHECK_FALSE(sut->get_rid());
     CHECK_FALSE(sut->has_alpha());
+}
+
+TEST_CASE("[AnimatedTexture] Set frames") {
+    const Ref<AnimatedTexture> sut = memnew(AnimatedTexture);
+
+    sut->set_frames(50);
+
+    REQUIRE_EQ(sut->get_frames(), 50);
 }
 
 TEST_CASE("[AnimatedTexture] Can't set 0 frames") {
@@ -58,7 +66,7 @@ TEST_CASE("[AnimatedTexture] Can't set 0 frames") {
     sut->set_frames(0);
     ERR_PRINT_ON;
 
-    CHECK_EQ(sut->get_frames(), 1);
+    REQUIRE_EQ(sut->get_frames(), 1);
 }
 
 TEST_CASE("[AnimatedTexture] Can't set more than max frames") {
@@ -69,6 +77,33 @@ TEST_CASE("[AnimatedTexture] Can't set more than max frames") {
     ERR_PRINT_ON;
 
     REQUIRE_EQ(sut->get_frames(), 1);
+}
+
+TEST_CASE("[AnimatedTexture] Set current frame") {
+    const Ref<AnimatedTexture> sut = memnew(AnimatedTexture);
+
+    sut->set_frames(50);
+    sut->set_current_frame(20);
+
+    REQUIRE_EQ(sut->get_current_frame(), 20);
+}
+
+TEST_CASE("[AnimatedTexture] Set pause") {
+    const Ref<AnimatedTexture> sut = memnew(AnimatedTexture);
+
+    sut->set_pause(true);
+    REQUIRE(sut->get_pause());
+    sut->set_pause(false);
+    REQUIRE_FALSE(sut->get_pause());
+}
+
+TEST_CASE("[AnimatedTexture] Set one shot") {
+    const Ref<AnimatedTexture> sut = memnew(AnimatedTexture);
+
+    sut->set_one_shot(true);
+    REQUIRE(sut->get_one_shot());
+    sut->set_one_shot(false);
+    REQUIRE_FALSE(sut->get_one_shot());
 }
 
 } // namespace TestAnimatedTexture
